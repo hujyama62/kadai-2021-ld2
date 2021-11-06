3d5714cbeaf2191a4ecb39210f61c41cd15e4ed8
hujyama62 <hujyama62@users.noreply.github.com>
2021-11-06 04:29:10 +0000

    今回の課題

---
+++ kadai-04/index1.js
@@ -0,0 +1,16 @@
+const product = (a, b) => {
+    console.log(a * b)
+}
+
+const sum = (n) => {
+    let conclution = 0
+
+    for (number=1; number <= n; number = number + 1 ) {
+        conclution = conclution + number 
+    }
+
+    console.log (conclution)
+}
+
+product(107, 109)
+sum(500)
